97170edfc6235812308f312d3b78666a31bdf662
EduardoGaray <garayarnaldos@gmail.com>
2022-01-05 16:56:13 +0100

    Initial commit

---
+++ src/main/java/com/telosabes/telosabes/TelosabesApplication.java
@@ -0,0 +1,13 @@
+package com.telosabes.telosabes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelosabesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelosabesApplication.class, args);
+	}
+
+}
